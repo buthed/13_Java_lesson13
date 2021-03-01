c1d19e84a82a8f4b9d06064011b761cd212968ea
buthed <temaTihonov@yandex.ru>
2021-03-01 20:48:18 +0300

    Ver 3

--- src/com/tematihonov/Car.java
+++ src/com/tematihonov/Car.java
@@ -1,41 +1,36 @@
 package com.tematihonov;
 
-import java.util.concurrent.CyclicBarrier;
+import static com.tematihonov.Main.PREPARING;
 
 public class Car implements Runnable {
     private static int CARS_COUNT;
     private Race race;
     private int speed;
     private String name;
-
     public String getName() {
         return name;
     }
-
     public int getSpeed() {
         return speed;
     }
-
     public Car(Race race, int speed) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
     }
-
     @Override
     public void run() {
-        CyclicBarrier cb = new CyclicBarrier(4);
-        new Thread(() -> {
-            try {
+        try {
             System.out.println(this.name + " готовится");
-            Thread.sleep(500 + (int) (Math.random() * 800));
+            Thread.sleep(500 + (int)(Math.random() * 800));
             System.out.println(this.name + " готов");
-            cb.await();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }).start();
+            PREPARING.countDown();
+            PREPARING.await();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
         }
--- src/com/tematihonov/Main.java
+++ src/com/tematihonov/Main.java
@@ -2,34 +2,35 @@ package com.tematihonov;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.CyclicBarrier;
 
 public class Main {
     public static final int CARS_COUNT = 4;
-        public static void main (String[]args){
+    protected static final CountDownLatch PREPARING = new CountDownLatch(CARS_COUNT);
+        public static void main (String[]args) throws InterruptedException{
             System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
             Race race = new Race(new Road(60), new Tunnel(), new Road(40));
             Car[] cars = new Car[CARS_COUNT];
             for (int i = 0; i < cars.length; i++) {
                 cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
             }
-//            CyclicBarrier cb = new CyclicBarrier(cars.length);
-//            for (int i = 0; i < cars.length; i++) {
-//                final int w = i;
-//                new Thread(() -> {
-//                    try {
-//                        new Thread(cars[w]).start();
-//                    } catch (Exception e) {
-//                        e.printStackTrace();
-//                    }
-//                }).start();
 
-
-            for (int i = 0; i < cars.length; i++) {
-                new Thread(cars[i]).start();
+            for (int i = 0; i < CARS_COUNT; i++) {
+                final int w = i;
+                new Thread(cars[w]).start();
             }
-            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
-            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+
+
+
+
+
+
+
+//            for (int i = 0; i < cars.length; i++) {
+//                new Thread(cars[i]).start();
+//            }
+//            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+//            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 }
-
